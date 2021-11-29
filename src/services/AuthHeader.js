92ec5eb962f4bf6dabd0c13a210eecfe224dc648
ekopurnama25 <54391297+ekopurnama25@users.noreply.github.com>
2021-11-29 14:43:07 +0700

    Update AuthHeader.js

--- src/services/AuthHeader.js
+++ src/services/AuthHeader.js
@@ -1,10 +1,8 @@
-//import AuthUsersServices from "./AuthUserServices";
 export default function authHeader() {
   const users =  JSON.parse(localStorage.getItem('users'));
 
-  console.log(users);
-
   if (user && user.accessToken) {
+    console.log(users.accessToken);
     return { Authorization: 'Bearer ' + user.accessToken };
   } else {
     console.log(user);
