c4c9d9795285a99f5f0570cf256f3b424eb12baa
ekopurnama25 <54391297+ekopurnama25@users.noreply.github.com>
2021-11-29 14:39:44 +0700

    Update AuthHeader.js

--- src/services/AuthHeader.js
+++ src/services/AuthHeader.js
@@ -1,13 +1,13 @@
 //import AuthUsersServices from "./AuthUserServices";
 export default function authHeader() {
-  const users = localStorage.getItem("users");
+  const users =  JSON.parse(localStorage.getItem('users'));
 
   console.log(users);
 
-  // if (user && user.accessToken) {
-  //   return { authorization: 'Bearer ' + user.accessToken };
-  // } else {
-  //   console.log(user);
-  //   return {};
-  // }
+  if (user && user.accessToken) {
+    return { Authorization: 'Bearer ' + user.accessToken };
+  } else {
+    console.log(user);
+    return {};
+  }
 }
